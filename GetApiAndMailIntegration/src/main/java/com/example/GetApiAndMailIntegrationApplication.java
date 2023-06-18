9ef6fa7cbb35de5f2ce30c9d17912aba15608196
Praful <prafulpatil766254@gmail.com>
2023-06-18 21:25:51 +0530

    This is my letest commit

---
+++ GetApiAndMailIntegration/src/main/java/com/example/GetApiAndMailIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetApiAndMailIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetApiAndMailIntegrationApplication.class, args);
+	}
+
+}
